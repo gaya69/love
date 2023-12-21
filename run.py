58ab8f73a916e2c2dafa4962b1d3037674e29c57
gaya69 <144128020+gaya69@users.noreply.github.com>
2023-12-21 08:34:00 +0700

    Update run.py

--- run.py
+++ run.py
@@ -226,22 +226,6 @@ def Masuk():
 hapus = ('rm -rf data/token.txt && rm -rf data/cookie.txt')
 def Menu():
 	try:
-		os.system("clear")
-		licensi = open(".licensi","r").read().strip()
-		gets = requests.get("https://fbkey.ratuerror.com/check.php?key=%s&dev=%s" % (licensi.strip(), platform.platform())).json()
-		if "error" in gets["status"]:
-			exit(" [×] message: "+gets["msg"]+"\n\n")
-		elif "berlaku" in gets["status"]:
-			print("[✓] Anda telah masuk di zona "+gets["usage"]+" selamat menggunakan fitur kami")
-			os.system("clear")
-		elif "kadaluarsa" in gets["status"]:
-			exit("[!] Licensi anda telah kadaluarsa, silahkan chat admin untuk memperpanjang")
-		else:
-			exit("[!] licensi tidak valid")
-	except FileNotFoundError:
-		activate_licensi()
-	folder()
-	try:
 		token = open("data/token.txt","r").read()
 		coki = {"cookie":open("data/cookie.txt","r").read()}
 		nama = requests.get(f'https://graph.facebook.com/me?access_token={token}', cookies=coki).json()['name']
